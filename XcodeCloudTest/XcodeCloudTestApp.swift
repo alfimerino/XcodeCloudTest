c4fc12da1dc8c341bdeff79e3a00c0bedc58167d
Alfredo Merino <alfimerino@gmail.com>
2024-02-29 09:52:53 -0500

    Initial Commit

---
+++ XcodeCloudTest/XcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestApp.swift
+//  XcodeCloudTest
+//
+//  Created by Alfredo Merino on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
